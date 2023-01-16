e733e2949ae12232e3b675066faf923334d41fad
Nishanth sairam <nishanthsairam@gmail.com>
2023-01-16 10:38:04 +0530

    Add Environment var

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -20,8 +21,7 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "littlesectretnewcreation";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 app.get("/", (req, res) => {
